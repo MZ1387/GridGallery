025ee4a3c1492d98adc2ceceeb7249d13891d7a7
Mario Zamora <maza1387@gmail.com>
2019-02-11 21:49:31 -0600

    Add cascade variable to TabLink

--- src/Gallery/Gallery.js
+++ src/Gallery/Gallery.js
@@ -1,38 +1,51 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import UserGrid from '../Profile/UserGrid';
 import { Image } from '../App';
 import Posts from '../Posts';
 
 const PhotoGrid = styled.div`
-  display: grid;
-  grid-template-columns: repeat(3, 305px);
-  gap: 20px;
-  justify-content: center;
+    display: grid;
+    grid-template-columns: repeat(3, 305px);
+    gap: 20px;
+    justify-content: center;
 `;
 
 const LinkGrid = styled.div`
     display: grid;
-    grid-template-columns: autp auto;
+    grid-template-columns: auto auto;
     justify-content: center;
+    gap: 20px;
+    margin-bottom: 20px
 `;
 
 const TabLink = styled(Link)`
-  text-decoration: none;
-  color: black;
-  width: 50px;
+    text-decoration: none;
+    color: #ccc;
+    text-transform: uppercase;
+    letter-spacing: 3px;
+    ${(props) => props.selected && css`
+        color: black;
+    `}
 `;
 
-export default ({ match }) => {
+export default ({ match, location }) => {
+    const cascade = location.search === '?type=cascade';
     return (
         <div>
         <UserGrid />
         <LinkGrid>
-            <TabLink to={`${match.url}/test1`} >
+            <TabLink 
+                selected={!cascade}
+                to={`${match.url}`} 
+            >
                 Square
             </TabLink>
-            <TabLink to={{pathname: `${match.url}`, search:"?type=cascade"}} >
+            <TabLink
+                selected={cascade}
+                to={{ pathname: `${match.url}`, search:"?type=cascade" }} 
+            >
                 Cascade
             </TabLink>
         </LinkGrid>
